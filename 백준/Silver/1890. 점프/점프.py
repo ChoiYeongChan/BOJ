152044ad38ab39ab8c58f80df94452d94b840da6
ChoiYeongChan <76672161+ChoiYeongChan@users.noreply.github.com>
2022-07-05 21:51:32 +0900

    [Silver I] Title: 점프, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.py
@@ -0,0 +1,15 @@
+import sys
+input=sys.stdin.readline
+n=int(input())
+a=[list(map(int,input().split())) for _ in range(n)]
+dp=[[0]*n for _ in range(n)]
+dp[0][0]=1
+for i in range(n):
+    for j in range(n):
+        if i==n-1 and j==n-1:
+            print(dp[i][j])
+            break
+        if j+a[i][j]<n:
+            dp[i][j+a[i][j]]+=dp[i][j]
+        if i + a[i][j]<n:
+            dp[i+a[i][j]][j]+=dp[i][j]
