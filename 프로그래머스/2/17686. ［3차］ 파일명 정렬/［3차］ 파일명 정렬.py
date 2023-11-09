3051f1ed23e2eed462ed8a1b4d71830a564650f8
ChoiYeongChan <76672161+ChoiYeongChan@users.noreply.github.com>
2023-11-09 22:05:27 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 4.99 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -0,0 +1,19 @@
+def solution(files):
+    answer = []
+    data=[]
+    for f in files:
+        head,number,tail='','',''
+        flag=False
+        for i in range(len(f)):
+            if f[i].isdigit():
+                number+=f[i]
+                flag=True
+            elif not flag:
+                head+=f[i]
+            else:
+                tail=f[i:]
+                break
+        answer.append((head,number,tail))
+    answer.sort(key=lambda x: (x[0].lower(),int(x[1])))
+    
+    return [''.join(t) for t in answer]
