9ac6761d90b27d26b78597c6102a703da77682ec
최영찬 <76672161+ChoiYeongChan@users.noreply.github.com>
2023-11-20 16:53:38 +0900

    [level 1] Title: 콜라 문제, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+    while(1){
+        if(n<a) break;
+        answer+=((n/a)*b);
+        n=(n/a)*b+n%a;
+    }
+    return answer;
+}
