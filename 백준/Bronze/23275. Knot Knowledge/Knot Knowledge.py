d02b48205e9eb0d3754269cb9b70c3b8ffe1a709
최영찬 <76672161+ChoiYeongChan@users.noreply.github.com>
2024-08-11 14:28:24 +0900

    [Bronze II] Title: Knot Knowledge, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/23275. Knot Knowledge/Knot Knowledge.py
@@ -0,0 +1,10 @@
+import sys
+input=sys.stdin.readline
+n = int(input())
+x_list = list(map(int, input().split()))
+y_list = list(map(int, input().split()))
+
+for x in x_list:
+    if x not in y_list:
+        print(x)
+        break
