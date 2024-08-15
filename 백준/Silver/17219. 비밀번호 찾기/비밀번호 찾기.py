7e175f8685ae7e4c83ba71015c3744371bee694d
최영찬 <76672161+ChoiYeongChan@users.noreply.github.com>
2024-08-15 10:51:19 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 192 ms, Memory: 49208 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -0,0 +1,9 @@
+import sys
+input=sys.stdin.readline
+n,m=map(int,input().split())
+dic={}
+for _ in range(n):
+    a,b=input().rstrip().split()
+    dic[a]=b
+for _ in range(m):
+    print(dic[input().rstrip()])
