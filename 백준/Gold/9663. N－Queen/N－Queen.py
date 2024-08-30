7a2c4a5f19a779cdb9ccd7778ee3e1c43772519f
최영찬 <76672161+ChoiYeongChan@users.noreply.github.com>
2024-08-30 16:06:44 +0900

    [Gold IV] Title: N-Queen, Time: 27160 ms, Memory: 198992 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -3,13 +3,12 @@ input=sys.stdin.readline
 n=int(input())
 ans=0
 row=[0]*n
-def is_promising(x):
+def is_queen(x):
     for i in range(x):
         if row[x]==row[i] or abs(row[x]-row[i])==abs(x-i):
             return False
     return True
-
-def n_queens(x):
+def nqueen(x):
     global ans
     if x==n:
         ans+=1
@@ -17,8 +16,7 @@ def n_queens(x):
     else:
         for i in range(n):
             row[x]=i
-            if is_promising(x):
-                n_queens(x+1)
-
-n_queens(0)
+            if is_queen(x):
+                nqueen(x+1)
+nqueen(0)
 print(ans)
