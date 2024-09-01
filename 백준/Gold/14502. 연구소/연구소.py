fa60ecb7cc49df92b81bbf2fa9a25d3e8a1cceaf
최영찬 <76672161+ChoiYeongChan@users.noreply.github.com>
2024-09-01 16:14:20 +0900

    [Gold IV] Title: 연구소, Time: 2704 ms, Memory: 127868 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,56 +1,43 @@
+import sys
 from collections import deque
 import copy
-
+input=sys.stdin.readline
+n,m=map(int,input().split())
+graph=[]
+dx=[0,0,-1,1]
+dy=[1,-1,0,0]
 def bfs():
-    queue = deque()
-    tmp_graph = copy.deepcopy(graph)
+    queue=deque()
+    tmp=copy.deepcopy(graph)
     for i in range(n):
         for j in range(m):
-            if tmp_graph[i][j] == 2:
-                queue.append((i, j))
-
+            if tmp[i][j]==2:
+                queue.append((i,j))
     while queue:
-        x, y = queue.popleft()
-
+        x,y=queue.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            if tmp_graph[nx][ny] == 0:
-                tmp_graph[nx][ny] = 2
-                queue.append((nx, ny))
-
-    global answer
-    cnt = 0
-
+            nx=x+dx[i]
+            ny=y+dy[i]
+            if 0<=nx<n and 0<=ny<m and tmp[nx][ny]==0:
+                tmp[nx][ny]=2
+                queue.append((nx,ny))
+    global ans
+    cnt=0
     for i in range(n):
-        cnt += tmp_graph[i].count(0)
-
-    answer = max(answer, cnt)
-
-
-def makeWall(cnt):
-    if cnt == 3:
+        cnt+=tmp[i].count(0)
+    ans=max(ans,cnt)
+def wall(cnt):
+    if cnt==3:
         bfs()
         return
-
     for i in range(n):
         for j in range(m):
-            if graph[i][j] == 0:
-                graph[i][j] = 1
-                makeWall(cnt+1)
-                graph[i][j] = 0
-
-n, m = map(int, input().split())
-graph = []
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-
-for i in range(n):
-    graph.append(list(map(int, input().split())))
-
-answer = 0
-makeWall(0)
-print(answer)
+            if graph[i][j]==0:
+                graph[i][j]=1
+                wall(cnt+1)
+                graph[i][j]=0
+for _ in range(n):
+    graph.append(list(map(int,input().split())))
+ans=0
+wall(0)
+print(ans)
